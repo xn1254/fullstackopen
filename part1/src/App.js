40018aa8b4e00f896447d9cc3d43890374908dcf
xunan <nan.xu@amh-group.com>
2022-01-09 16:48:26 +0800

    测试

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,69 +1,48 @@
 /*
  * @Author: your name
  * @Date: 2021-11-14 22:20:59
- * @LastEditTime: 2022-01-09 15:39:47
+ * @LastEditTime: 2022-01-09 16:43:24
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \全栈课程作业\part1\src\App.js
  */
-import React from 'react'
+import React, { useState } from 'react'
 
-const course = {
-  name: 'Half Stack application development',
-  part: [
-    {
-      part: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      part: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      part: 'State of a component',
-      exercises: 14
-    }
-  ]
-}
-
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.partData.part} {props.partData.exercises}
-    </p>
-  )
-}
-
-const Content = (props) => {
+const Statistics = ({good, neutral, bad}) => {
+  if (good + neutral + bad === 0) {
+    return (
+      <p>no data</p>
+    )
+  }
   return (
     <div>
-      {
-        props.parts.map((item, index) => 
-          <Part key={index} partData={item}></Part>
-        )
-      }
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive {good / (good + neutral + bad) * 100}%</p>
     </div>
   )
 }
 
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)}</p>
-  )
-}
-
 const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const clickGood = () => setGood(good + 1)
+  const clickNeutral = () => setNeutral(neutral + 1)
+  const clickBad = () => setBad(bad + 1)
+
   return (
     <div>
-      <Header course={course.name}></Header>
-      <Content parts={course.part}></Content>
-      <Total parts={course.part}></Total>
+      <p>give feedback</p>
+      <button onClick={clickGood}>good</button>
+      <button onClick={clickNeutral}>neutral</button>
+      <button onClick={clickBad}>bad</button>
+      <p>statistics</p>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   )
 }
