b7ff1f37b8f74f2a214e9231dd697c17e5aff826
xunan <nan.xu@amh-group.com>
2022-01-09 15:08:35 +0800

    文件暂存

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React from 'react'
 
-const Hello = (props) => {
+const course = 'Half Stack application development'
+const partName = {
+  part1: 'Fundamentals of React',
+  part2: 'Using props to pass data',
+  part3: 'State of a component'
+}
+const exercisesCount = {
+  exercises1: 10,
+  exercises2: 7,
+  exercises3: 14
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.partData.part} {props.partData.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
   return (
     <div>
-      <p>hello {props.name}</p>
+      <Part partData={partData1}></Part>
+      <Part partData={partData2}></Part>
+      <Part partData={partData3}></Part>
     </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercisesCount.exercises1 + props.exercisesCount.exercises2 + props.exercisesCount.exercises3}</p>
   )
 }
 
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-
   return (
     <div>
-      <p>Hello world it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <Header course={course}></Header>
+      <Content partName={partName} exercisesCount={exercisesCount}></Content>
+      <Total exercisesCount={exercisesCount}></Total>
     </div>
   )
 }
 
-export default App;
+export default App
